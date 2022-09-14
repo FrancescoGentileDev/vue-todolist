578f1cb5f9ccc71a75aa1e9059decf358f1db9cb
svalvastic <assogames2@gmail.com>
2022-09-14 16:52:28 +0200

    ✨ feat(priority): add priority button

--- javascript/script.js
+++ javascript/script.js
@@ -28,10 +28,7 @@ const app = new Vue({
     todos: data,
     valueInput: "",
   },
-    methods: {
-      
-
-        
+  methods: {
     removeTodo(index) {
       this.todos.splice(index, 1);
     },
@@ -43,10 +40,29 @@ const app = new Vue({
       if (stringClean) {
         this.todos.push(new Todo(stringClean));
         this.valueInput = "";
-        }
-      else {
-          console.error("PASSAGGIO STRINGA VUOTA");
-        }
+      } else {
+        console.error("PASSAGGIO STRINGA VUOTA");
+      }
+    },
+
+    goUpTodo(index) {
+      let tempTodoElement = this.todos[index];
+
+      if (index > 0) {
+        this.todos.splice(index, 1);
+        this.todos.splice(index - 1, 0, tempTodoElement);
+      }
+
+      console.log(tempTodoElement, this.todos);
+    },
+    goDownTodo(index) {
+      let tempTodoElement = this.todos[index];
+
+      if (index < this.todos.length) {
+        this.todos.splice(index, 1);
+        this.todos.splice(index + 1, 0, tempTodoElement);
+      }
+      console.log(tempTodoElement, this.todos);
     },
   },
 });
